1d3052fb384296097cb738e76f9cf9582c522a6b
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-08-01 11:27:55 -0700

    populating front page with last workout data from seeder

--- server.js
+++ server.js
@@ -23,10 +23,27 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-//create a single hello world route
-app.get("*", (req, res)=>{
-    res.send("<h1>Hello, World!</h1>");
-});
+//respond to post request 
+app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  //respond to post request 
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
 //listen 
 app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`));
