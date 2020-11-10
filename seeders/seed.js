d52b2d889f478bccfc1b788460e3fb8d2758dcbe
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-11-10 12:19:43 -0800

    added Mondb connection string to seeds file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGO_ATLAS || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
